ec6729eb7deda1e03b212b68e3b6505011b1ea63
Ilya <h82st8@gmail.com>
2019-12-13 13:33:42 +0300

    4

--- js/script.js
+++ js/script.js
@@ -17,10 +17,18 @@ $('.owl-carousel').owlCarousel({
 
 let btn = document.getElementById("btn");
 let form = document.getElementById("form");
+let close = document.getElementById("close");
+
 
 btn.addEventListener("click", func);
+close.addEventListener("click", fun);
 
 function func(event) {
     console.log(event);
     form.classList.toggle("form_active");
+}
+
+function fun(event) {
+    console.log(event);
+    form.classList.remove("form_active");
 }
